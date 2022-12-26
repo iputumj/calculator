74abe450c3839ff090e3db3cb9281d190115ebe6
iputumj <iputu.mj@gmail.com>
2022-12-26 18:56:35 +0700

    add state, function handleClick, handleClear, handleBackspace and calculate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+
+  const handleClear = () => {
+    setResult("")
+  }
+
+  const handleBackspace = () => {
+    setResult(result.slice(0, -1));
+  }
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error")
+    }
+  }
+
   return (
     <>
     <div className="container">
       <form>
-        <input type="text" />
+        <input type="text" value={result} />
       </form>
 
       <div className="keypad">
-        <button>AC</button>
-        <button>C</button>
-        <button>/</button>
-        <button>*</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>=</button>
-        <button>0</button>
-        <button>.</button>
+        <button onClick={handleClear} className="button">AC</button>
+        <button onClick={handleBackspace} className="button">C</button>
+        <button onClick={handleClick} className="button" name="/">/</button>
+        <button onClick={handleClick} className="button" name="*">*</button>
+        <button onClick={handleClick} className="button" name="7">7</button>
+        <button onClick={handleClick} className="button" name="8">8</button>
+        <button onClick={handleClick} className="button" name="9">9</button>
+        <button onClick={handleClick} className="button" name="-">-</button>
+        <button onClick={handleClick} className="button" name="4">4</button>
+        <button onClick={handleClick} className="button" name="5">5</button>
+        <button onClick={handleClick} className="button" name="6">6</button>
+        <button onClick={handleClick} className="button" name="+">+</button>
+        <button onClick={handleClick} className="button" name="1">1</button>
+        <button onClick={handleClick} className="button" name="2">2</button>
+        <button onClick={handleClick} className="button" name="3">3</button>
+        <button onClick={calculate} className="button" name="=">=</button>
+        <button onClick={handleClick} className="button" name="0">0</button>
+        <button onClick={handleClick} className="button" name=".">.</button>
       </div>
     </div>
   </>
