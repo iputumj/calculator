e971eac476b0ec3bbe9c5ebd539abad9210c4a5b
iputumj <iputu.mj@gmail.com>
2022-12-26 19:45:17 +0700

    update styles

--- src/App.js
+++ src/App.js
@@ -32,23 +32,23 @@ function App() {
       </form>
 
       <div className="keypad">
-        <button onClick={handleClear} className="button">AC</button>
-        <button onClick={handleBackspace} className="button">C</button>
-        <button onClick={handleClick} className="button" name="/">/</button>
-        <button onClick={handleClick} className="button" name="*">*</button>
+        <button onClick={handleClear} className="highlight" id="button-clear">AC</button>
+        <button onClick={handleBackspace} className="highlight" id="button-backspace">CE</button>
+        <button onClick={handleClick} className="highlight" name="/">/</button>
+        <button onClick={handleClick} className="highlight" name="*">*</button>
         <button onClick={handleClick} className="button" name="7">7</button>
         <button onClick={handleClick} className="button" name="8">8</button>
         <button onClick={handleClick} className="button" name="9">9</button>
-        <button onClick={handleClick} className="button" name="-">-</button>
+        <button onClick={handleClick} className="highlight" name="-">-</button>
         <button onClick={handleClick} className="button" name="4">4</button>
         <button onClick={handleClick} className="button" name="5">5</button>
         <button onClick={handleClick} className="button" name="6">6</button>
-        <button onClick={handleClick} className="button" name="+">+</button>
+        <button onClick={handleClick} className="highlight" name="+">+</button>
         <button onClick={handleClick} className="button" name="1">1</button>
         <button onClick={handleClick} className="button" name="2">2</button>
         <button onClick={handleClick} className="button" name="3">3</button>
-        <button onClick={calculate} className="button" name="=">=</button>
-        <button onClick={handleClick} className="button" name="0">0</button>
+        <button onClick={calculate} className="highlight" name="=" id="button-result">=</button>
+        <button onClick={handleClick} className="button" name="0" id="button-0">0</button>
         <button onClick={handleClick} className="button" name=".">.</button>
       </div>
     </div>
