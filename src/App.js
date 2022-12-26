ab7237fc9da23078a2144d583477425228cf9810
iputumj <iputu.mj@gmail.com>
2022-12-26 17:34:16 +0700

    add button layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <div className="container">
+      <form>
+        <input type="text" />
+      </form>
+
+      <div className="keypad">
+        <button>AC</button>
+        <button>C</button>
+        <button>/</button>
+        <button>*</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>=</button>
+        <button>0</button>
+        <button>.</button>
+      </div>
+    </div>
+  </>
   )
 }
 
